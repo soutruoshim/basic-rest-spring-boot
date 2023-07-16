4a18209a78f0ad0dd816b7365c5dd8a2e2a9e8bf
soutruoshim <sout.rahim@gmail.com>
2023-07-16 21:05:09 +0700

    init

---
+++ src/main/java/com/srhdp/basicrestspringboot/BasicRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.srhdp.basicrestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srhdp/basicrestspringboot/BasicRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.basicrestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
